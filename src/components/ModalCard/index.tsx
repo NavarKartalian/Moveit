5fbb7c78498cd02af873bf89d55fbc5ee8a6395a
Navar Kartalian <alpha.hunter88@gmail.com>
2021-11-09 15:01:22 -0300

    Notification

--- src/components/ModalCard/index.tsx
+++ src/components/ModalCard/index.tsx
@@ -33,15 +33,15 @@ export function ModalCard() {
     <Flex position='absolute'>
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay />
-        <ModalContent maxW='398px' height='340px'>
+        <ModalContent maxW={{'base': '300px', 'lg': '398px'}} height={{'base': '260px', 'lg': '340px'}}>
           <ModalCloseButton />
           <ModalBody backgroundColor={bg} borderRadius='lg'>
             <Flex direction='column' align='center'>
               <Text 
                 color='#5965E0' 
-                minW='160px' 
-                minH='180px'
-                fontSize='140px' 
+                minW={{'base': '130px', 'lg': '160px'}} 
+                minH={{'base': '150px', 'lg': '180px'}}
+                fontSize={{'base': '100px', 'lg': '140px'}} 
                 backgroundImage='images/Union.png' 
                 backgroundPosition='center' 
                 backgroundSize='contain' 
@@ -51,7 +51,7 @@ export function ModalCard() {
                 {level}
               </Text>
               <Text color={colorMode === 'light' ? '#2E384D' : 'white'} fontSize='30' fontWeight='600'>Parabéns</Text>
-              <Text color={color} fontSize='20'>Você alcançou um novo level.</Text>
+              <Text color={color} fontSize={{'base': '16', "lg": '20'}}>Você alcançou um novo level.</Text>
             </Flex>
           </ModalBody>
         </ModalContent>
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -68,6 +68,8 @@ export function ChallengesProvider({ children, ...rest }: ChallengeProviderProps
 
         setActiveChallenge(challenge);
 
+        new Audio('/notification.mp3').play();
+
         if (Notification.permission === 'granted') {
             new Notification('Novo desafio ', {
                 body: `Valendo ${challenge.amount} xp.`,
