1c978e19e022e76131c8e6f180e4cc565633d580
Navar Kartalian <alpha.hunter88@gmail.com>
2021-11-15 23:03:09 -0300

    SEO e Meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,10 @@ import { theme } from '../styles/theme';
 export default class MyDocument extends Document {
     render() {
         return(
-            <Html lang="en">
+            <Html lang="pt-br">
                 <Head>
+                    <meta name="author" content="Navar Kartalian" />
+                    <meta name="description" content="Uma aplicação para estimular a movimentação de pessoas que passam muito tempo trabalhando em frente ao computador" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
                     <link rel="shortcut icon" href="/favicon.png" type="image/png" />
